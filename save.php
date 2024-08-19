00726736fdc1b14d57998f376d048b70ab6c3e64
jutbone <aliu79709@gmail.com>
2024-08-19 09:08:56 +0500

    Insert data through save.php file

--- save.php
+++ save.php
@@ -1,10 +1,32 @@
 <?php
 
+$host="localhost";
+$username="root";
+$password="";
+$dbname="phpcrud";
+
+$connection= new mysqli($host,$username,$password,$dbname);
+
+if(!$connection){
+  echo "Connection Not Established";
+}
+
+
 $name=$_POST['name'];
 $email=$_POST['email'];
 $phone=$_POST['phone'];
 $dob=$_POST['dob'];
 $gender=$_POST['gender'];
+
+
+$query="Insert into students(name,email,phone,dob,gender) values ('$name','$email',
+'$phone','$dob','$gender')";
+
+if(mysqli_query($connection,$query)){
+   echo "Data Saved Successfully.";
+}else{
+    echo "Something went wrong.";
+}
 
 
 
