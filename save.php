e660b53244cb4a00e3f9b0007361442497aece41
jutbone <aliu79709@gmail.com>
2024-08-19 09:31:29 +0500

    Apply validation and Success and warning messages in save functionality]

--- save.php
+++ save.php
@@ -13,22 +13,48 @@ if(!$connection){
 
 
 $name=$_POST['name'];
-$email=$_POST['email'];
-$phone=$_POST['phone'];
-$dob=$_POST['dob'];
-$gender=$_POST['gender'];
 
+if($_POST['name'] =='' ){
+    http_response_code(404);
+    echo json_encode(array("message" => "Please Enter Name First."));
+    exit();
+}
+$email=$_POST['email'];
+if($_POST['email'] =='' ){
+    http_response_code(404);
+    echo json_encode(array("message" => "Please Enter Email First."));
+    exit();
+}
+$phone=$_POST['phone'];
+if($_POST['phone'] =='' ){
+    http_response_code(404);
+    echo json_encode(array("message" => "Please Enter Phone no First."));
+    exit();
+}
+$dob=$_POST['dob'];
+if($_POST['dob'] =='' ){
+    http_response_code(404);
+    echo json_encode(array("message" => "Please Enter DoB First."));
+    exit();
+}
+$gender=$_POST['gender'];
+if($_POST['gender'] =='' ){
+    http_response_code(404);
+    echo json_encode(array("message" => "Please Enter Gender First."));
+    exit();
+}
 
 $query="Insert into students(name,email,phone,dob,gender) values ('$name','$email',
 '$phone','$dob','$gender')";
 
 if(mysqli_query($connection,$query)){
-   echo "Data Saved Successfully.";
+   http_response_code(200);
+   echo json_encode(array("message" => "Data Saved Successfully."));
+   exit();
 }else{
-    echo "Something went wrong.";
+    http_response_code(300);
+    echo json_encode(array("message" => "Something Went Wrong."));
+    exit();
 }
-
-
-
 
 ?>
